6022aaa6dfa194a21115355af9553c431eb3dfb4
Th3Manuel <romeropallaresmanueldavid11@gmail.com>
2023-10-14 16:23:35 -0500

    update 1

--- src/main/java/Main/Proyectoaula.java
+++ src/main/java/Main/Proyectoaula.java
@@ -1,6 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package Main;
 
@@ -18,3 +15,6 @@ public class Proyectoaula {
     }
 
     }
+
+
+aout 
